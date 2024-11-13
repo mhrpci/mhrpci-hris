66e758e20ad091f7454a402e2ba6288bf6e1c40e
mhrpci <mhrpciofficial@gmail.com>
2024-11-13 15:54:54 +0800

    Updated

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            SubsidiarySeeder::class,
             SectionSeeder::class,
             PermissionSeeder::class,
             RoleSeeder::class,
