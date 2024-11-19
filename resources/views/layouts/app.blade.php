8081cd0decf6ceee0794cbe9b9363a253380ac1c
mhrpci <159887622+mhrpci@users.noreply.github.com>
2024-11-19 09:26:36 +0800

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>MHR Property COnglomerates, Inc.</title>
+    <title>MHR Property Conglomerates, Inc.</title>
 <style>
       /* Loader */
       .loader {
